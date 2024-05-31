7110a9fea8429b81dae416c65977dcafca7c6f82
Nsloni <154657596+Nsloni@users.noreply.github.com>
2024-05-31 19:15:37 +0530

    Add files via upload
    added pandas

--- Basic.py
+++ Basic.py
@@ -1,39 +1,95 @@
-# 1.NORMAL PYTHON
+# Instalation
 
-'''l1=[10,20,30,40]
-l2=[11,22,33,44,55]
-print(l1+l2)
+import pandas as pd
 
-#OUTPUT:   [10, 20, 30, 40, 11, 22, 33, 44, 55]
+# Syntax:
 
-# 2.using numpy
-
-import numpy as np
-
-l1=np.array([10,20,30,40])
-l2=np.array([60,70,60,50])
-print(l1)
-print(l2)
-print(l1+l2)
-
-DIFF_DATA_TYPES=np.array([12,12.12,"nagaraj"])
-print(DIFF_DATA_TYPES)'''  # ['12' '12.12' 'nagaraj']
-
-#OUTPUT: [70 90 90 90]
-
-# 3.NORMAL PYTHON
-
-'''l1=([[11,22,33],[10,20,30]])
-print(l1)
-#OUTPUT: [[11, 22, 33], [10, 20, 30]]
-
-# 4.using numpy
-
-import numpy as np
-a=np.array([[11,22,33],[10,20,30]])
-print(a)'''
-#OUTPUT:[[11 22 33]
-#OUTPUT: [10 20 30]]
+import pandas as pd
+a = pd.Series()
+print(a)
 
 
-13:20
+# Start Arrray
+
+import pandas as pd
+
+a = [1,2,3,4,5]
+
+res = pd.Series(a)
+print(res)
+print(type(res))
+
+#OUTPUT:
+'''
+Trusted
+
+Code
+
+JupyterLab
+
+Python 3 (ipykernel)'''
+
+# Syntax:
+
+import pandas as pd
+a = pd.Series()
+print(a)
+Series([], dtype: object)
+Selection deleted'''
+
+
+# Start Arrray
+
+import pandas as pd
+
+a = [1,2,3,4,5]
+
+res = pd.Series(a)
+print(res)
+print(type(res))
+0    1
+1    2
+2    3
+3    4
+4    5
+dtype: int64
+<class 'pandas.core.series.Series'>'''
+
+
+# Tables Like row & col
+
+dic = {"name":['nagaraj','ns','loni'], "age":[12,12,23,345], "rank" : [1,2,3,4]}
+
+res = pd.Series(dic)
+print(res)
+
+# OUTPUT:
+'''name    [nagaraj, ns, loni]
+age       [12, 12, 23, 345]
+rank           [1, 2, 3, 4]
+dtype: object'''
+
+
+# Same value but Different Index
+
+arar = pd.Series(12,index=[1,2,3,4,5,6,7,8,9])
+print(arar)
+
+# OUTPUT:
+'''1    12
+2    12
+3    12
+4    12
+5    12
+6    12
+7    12
+8    12
+9    12
+dtype: int64'''
+
+
+
+
+
+
+
